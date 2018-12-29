f78b2efe66e81cff234b09254c166b23cdf042ff
Ben Forster <forsteremailaddress@gmail.com>
2018-12-29 17:52:06 +0000

    Update script.js

--- discord.js/script.js
+++ discord.js/script.js
@@ -1,1 +1,8 @@
+const DW = require('DW');
+const DWClient = new DW(`${client.user.id}`, 'TOKEN_HERE');
 
+DWCLIENT.updateCount(client.guilds.size).then(() => {
+    console.log('Server Count Posted!')
+}).catch((e) => {
+    console.error(e)
+})
